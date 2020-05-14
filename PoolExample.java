81c1b5c86064d272dfee8fb5cb60c4ea8fbc8a80
Ekaterina <50626720+Sguhaaa@users.noreply.github.com>
2020-05-14 15:53:14 +0500

    Add files via upload

---
+++ PoolExample.java
@@ -0,0 +1,71 @@
+/*
+Цитата:
+Говоря о ThreadPoolExecutor'ах, стоит отметить интересные особенности при работе.
+Например, нельзя посылать задачи в ThreadPoolExecutor, если там нет места.
+
+Хокку описывающее проблему:
+
+С треском код упал:
+Очередь закончилась.
+Я пробудился вдруг.
+
+Данная ситуация встречается в нашей задаче. Необходимо создать обработчик непринятых задач.
+ */
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PoolExample {
+
+    private static class CallerRunsPolicy implements RejectedExecutionHandler {
+
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+            if (!e.isShutdown()) {
+                System.out.println("job restart");
+                r.run();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // создаем пул для выполнения наших задач
+        // максимальное количество созданных задач - 3
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                // не изменяйте эти параметры
+                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
+
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+
+        // сколько задач выполнилось
+        AtomicInteger count = new AtomicInteger(0);
+
+        // сколько задач выполняется
+        AtomicInteger inProgress = new AtomicInteger(0);
+
+        // отправляем задачи на выполнение
+        for (int i = 0; i < 30; i++) {
+            final int number = i;
+            Thread.sleep(10);
+
+            System.out.println("creating #" + number);
+            executor.submit(() -> {
+                int working = inProgress.incrementAndGet();
+                System.out.println("start #" + number + ", in progress: " + working);
+                try {
+                    // тут какая-то полезная работа
+                    Thread.sleep(Math.round(1000 + Math.random() * 2000));
+                } catch (InterruptedException e) {
+                    // происходит игноринг
+                }
+                working = inProgress.decrementAndGet();
+                System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
+                return null;
+            });
+        }
+        executor.shutdown();
+    }
+}
